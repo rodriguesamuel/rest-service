1e6450757b67288a7b230484232c0bb9d3ed5aee
Samuel Rodrigues <samuel.bruno.aguiar@gmail.com>
2024-02-26 23:36:14 -0300

    Update GreetingController.java

--- src/main/java/com/example/restservice/greeting/GreetingController.java
+++ src/main/java/com/example/restservice/greeting/GreetingController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s! Test.";
+	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "sam") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
